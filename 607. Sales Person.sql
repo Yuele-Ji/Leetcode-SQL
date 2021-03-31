2d5afd5ca41cdaaf45b59ef32710a22c8961e8df
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-03-31 16:29:01 -0400

    Create 607. Sales Person.sql

---
+++ 607. Sales Person.sql
@@ -0,0 +1,7 @@
+SELECT name
+FROM salesperson
+WHERE sales_id NOT IN(SELECT o.sales_id
+                      FROM orders o
+                      JOIN company c
+                      ON c.com_id = o.com_id
+                      WHERE c.name = 'RED')

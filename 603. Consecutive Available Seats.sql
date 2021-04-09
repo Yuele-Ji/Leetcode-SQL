96f8e47e654f2df0088f62b41f2e18114be21689
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-04-08 22:42:04 -0400

    Update 603. Consecutive Available Seats.sql

--- 603. Consecutive Available Seats.sql
+++ 603. Consecutive Available Seats.sql
@@ -1,7 +1,9 @@
 SELECT seat_id
 FROM(SELECT *,
-       LAG(free, 1)OVER() AS LAST,
-       LEAD(free,1)OVER() AS NEXT
+       LAG(free, 1)OVER(ORDER BY seat_id) AS LAST,
+       LEAD(free,1)OVER(ORDER BY seat_id) AS NEXT
        FROM cinema) AS SEAT
 WHERE free = 1 AND (LAST = 1 OR NEXT =1)
 ORDER BY seat_id
+
+--2. SELF JOIN ON ABS(A.ID - B.ID) =1

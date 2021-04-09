87b0794e073d159d4980f47d2623a2b5f59791e2
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-04-08 21:54:12 -0400

    Update 586. Customer Placing the Largest Number of Orders.sql

--- 586. Customer Placing the Largest Number of Orders.sql
+++ 586. Customer Placing the Largest Number of Orders.sql
@@ -13,3 +13,10 @@ HAVING COUNT(*) = (SELECT COUNT(*) FROM Orders
                    GROUP BY customer_number 
                    ORDER BY 1 DESC
                    LIMIT 1)
+                   
+--3.
+SELECT customer_number FROM (
+SELECT customer_number, RANK()OVER(ORDER BY COUNT(order_number) DESC) AS RNK
+FROM Orders
+GROUP BY customer_number) T
+WHERE RNK = 1

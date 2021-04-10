1f01cfda733cffef0523246c9cf4f73de3f00265
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-04-10 16:51:10 -0400

    Update and rename 607. Sales Person.sql to **607. Sales Person.sql

--- **607. Sales Person.sql
+++ **607. Sales Person.sql
@@ -5,3 +5,5 @@ WHERE sales_id NOT IN(SELECT o.sales_id
                       JOIN company c
                       ON c.com_id = o.com_id
                       WHERE c.name = 'RED')
+                      
+-- You need to query who sold to company 'RED' first, then output the sales person who is not in the first query result.

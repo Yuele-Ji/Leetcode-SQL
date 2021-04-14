21324fc363e928a97f117ce6092fe1a68c991844
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-04-14 17:22:35 -0400

    Update *1084. Sales Analysis III.sql

--- *1084. Sales Analysis III.sql
+++ *1084. Sales Analysis III.sql
@@ -6,4 +6,21 @@ FROM Sales s
 JOIN Product p
 ON s.product_id = p.product_id
 GROUP BY p.product_id, p.product_name
-HAVING start_date >= '2019-01-01' AND end_date <= '2019-03-31') tmp
+HAVING start_date >= '2019-01-01' AND end_date <= '2019-03-31') tmp.
+
+--2.
+SELECT p.product_id, p.product_name
+FROM Product p
+JOIN Sales s
+ON s.product_id = p.product_id
+GROUP BY p.product_id
+HAVING SUM(CASE WHEN s.sale_date BETWEEN '2019-01-01' AND '2019-03-31' THEN 1 ELSE 0 END) > 0
+AND SUM(CASE WHEN s.sale_date < '2019-01-01' OR s.sale_date > '2019-03-31' THEN 1 ELSE 0 END) = 0
+
+--3
+SELECT p.product_id, p.product_name
+FROM Product p
+JOIN Sales s
+ON s.product_id = p.product_id
+GROUP BY p.product_id
+HAVING MIN(s.sale_date) >= '2019-01-01' AND MAX(s.sale_date) <= '2019-03-31'

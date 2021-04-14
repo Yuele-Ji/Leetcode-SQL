966b533efeb45f8555416390a33aeb202dfeac43
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-04-14 17:10:09 -0400

    Update 1083. Sales Analysis II.sql

--- 1083. Sales Analysis II.sql
+++ 1083. Sales Analysis II.sql
@@ -8,15 +8,10 @@ FROM t
 WHERE product_name = 'S8' AND buyer_id NOT IN (SELECT buyer_id FROM t WHERE product_name = 'iPhone')
 
 --2. 
-SELECT DISTINCT buyer_id
-FROM Sales
-WHERE buyer_id IN (SELECT s.buyer_id
-                   FROM Sales s
-                   JOIN Product p
-                   ON s.product_id = p.product_id
-                   WHERE product_name = 'S8')
-AND buyer_id NOT IN (SELECT s.buyer_id
-                  FROM Sales s
-                  JOIN Product p
-                  ON s.product_id = p.product_id
-                  WHERE product_name = 'iPhone')
+SELECT s.buyer_id
+FROM Sales s
+JOIN Product p
+ON s.product_id = p.product_id
+GROUP BY s.buyer_id
+HAVING SUM(CASE WHEN p.product_name = 'S8' THEN 1 ELSE 0 END) > 0
+AND SUM(CASE WHEN p.product_name = 'iPhone' THEN 1 ELSE 0 END) = 0

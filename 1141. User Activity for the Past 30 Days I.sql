e7b97425d8bf0827e1df03dfe31d064887dde576
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-04-15 11:51:14 -0400

    Update 1141. User Activity for the Past 30 Days I.sql

--- 1141. User Activity for the Past 30 Days I.sql
+++ 1141. User Activity for the Past 30 Days I.sql
@@ -1,4 +1,5 @@
 SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
 FROM Activity
 WHERE DATEDIFF('2019-07-27', activity_date) <30
-GROUP BY activity_date
+GROUP BY activity_date 
+-- HAVING COUNT(activity_type) >= 1 is unnecessary since the activity_type column is an ENUM of 4 types, it has to have at least one activity to be recoreded.

51b02a9e8cb8614ed2ff6d14ef1501918d4eb79d
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-04-15 12:03:12 -0400

    Update 1142. User Activity for the Past 30 Days II.sql

--- 1142. User Activity for the Past 30 Days II.sql
+++ 1142. User Activity for the Past 30 Days II.sql
@@ -1,3 +1,3 @@
-SELECT COALESCE(ROUND(COUNT(DISTINCT session_id) / COUNT(DISTINCT user_id), 2), 0) AS average_sessions_per_user
+SELECT ROUND(COALESCE(COUNT(DISTINCT user_id, session_id) / COUNT(DISTINCT user_id), 0), 2) AS average_sessions_per_user
 FROM Activity
 WHERE DATEDIFF('2019-07-27', activity_date) < 30

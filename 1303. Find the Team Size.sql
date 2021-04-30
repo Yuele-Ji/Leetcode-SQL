8769bfa3fb63e23f9fc9abefce5c34cb85c6276a
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-04-29 22:43:59 -0400

    Update 1303. Find the Team Size.sql

--- 1303. Find the Team Size.sql
+++ 1303. Find the Team Size.sql
@@ -1,2 +1,11 @@
 SELECT employee_id, COUNT(employee_id)OVER(PARTITION BY team_id) AS team_size
 FROM Employee
+
+--2.left join
+SELECT e.employee_id, a.team_size
+FROM Employee e
+LEFT JOIN
+(SELECT team_id, COUNT(employee_id) AS team_size
+FROM Employee
+GROUP BY team_id) a
+ON e.team_id = a.team_id

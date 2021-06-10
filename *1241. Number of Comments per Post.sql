83739e0b1110046aca8d1cdea1c34238c6995624
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-06-09 21:04:02 -0400

    Create *1241. Number of Comments per Post.sql

---
+++ *1241. Number of Comments per Post.sql
@@ -0,0 +1,7 @@
+SELECT a.sub_id AS post_id, COUNT(DISTINCT b.sub_id) AS number_of_comments
+FROM Submissions a
+LEFT JOIN Submissions b
+ON a.sub_id = b.parent_id
+WHERE a.parent_id IS NULL
+GROUP BY 1
+ORDER BY 1

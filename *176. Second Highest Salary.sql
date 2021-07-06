418917dc6955f7d6d13cf91e2bdfcf90d15e3ed8
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-07-06 12:52:34 -0400

    Update *176. Second Highest Salary.sql

--- *176. Second Highest Salary.sql
+++ *176. Second Highest Salary.sql
@@ -10,5 +10,14 @@ LIMIT 1 OFFSET 1), NULL) AS SecondHighestSalary
 -- LIMIT 1 OFFSET 1 to skip the frst one and get second one
 
 --2.
-SELECT MAX(Salary) AS SecondHighestSalary FROM Employee 
+SELECT MAX(Salary) AS SecondHighestSalary 
+FROM Employee 
 WHERE Salary < (SELECT MAX(Salary) FROM Employee);
+
+--3.
+WITH GetNTHSalary AS (SELECT Salary, DENSE_RANK()OVER(ORDER BY Salary DESC) AS rnk
+                      FROM Employee)
+SELECT MAX(Salary) AS GetNTHSalary
+FROM GETNTHSALARY
+WHERE rnk = 2
+-- we coud use both distinct / max() here since we just trying to avoid duplicates. but remember to use coalesce with distinct.

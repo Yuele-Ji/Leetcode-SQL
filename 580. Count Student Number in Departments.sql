74c2ec11e7bea02bad60cc4b6d5831bf020f7944
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-07-21 18:12:01 -0400

    Update 580. Count Student Number in Departments.sql

--- 580. Count Student Number in Departments.sql
+++ 580. Count Student Number in Departments.sql
@@ -1,6 +1,15 @@
+--1. coalesce()
 SELECT d.dept_name, COALESCE(COUNT(s.student_id),0) AS student_number
 FROM department d
 LEFT JOIN student s
 ON s.dept_id = d. dept_id
 GROUP BY d.dept_name
+ORDER BY 2 DESC, 1
+
+--2.sum treat null as 0
+SELECT dept_name, SUM(CASE WHEN student_id THEN 1 ELSE 0 END) AS student_number
+FROM department d
+LEFT JOIN student s
+ON s.dept_id = d. dept_id
+GROUP BY dept_name
 ORDER BY 2 DESC, 1

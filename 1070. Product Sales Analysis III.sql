cf409f7f55f8ec653e9a5b1f990904f5dc753977
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-07-26 18:23:00 -0400

    Update 1070. Product Sales Analysis III.sql

--- 1070. Product Sales Analysis III.sql
+++ 1070. Product Sales Analysis III.sql
@@ -6,5 +6,11 @@ WHERE RNK = 1
 
 --2.
 SELECT product_id, year AS first_year, quantity, price
-FROM Sales
+FROM Sales]
 WHERE (product_id, year) IN (SELECT product_id, MIN(year) FROM Sales GROUP BY product_id)
+
+-- If you do:
+--ELECT product_id, MIN(year) AS first_year, quantity, price
+--FROM Sales a
+--GROUP BY product_id
+--Then unaggregated columns, e.g., quantity and price will not match with the MIN(year) of each product_id. 
